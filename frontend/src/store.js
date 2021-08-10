dfe3cdc8e58bcfa414f0442735cfb3cd55ec59b8
henry pvt <gshokx91@gmail.com>
2021-08-10 12:09:55 +0200

    Implement Place order Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
+import { orderCreateReducer } from "./reducers/orderReducers";
 import {
   productDetailsReducer,
   productListReducer,
@@ -9,6 +10,7 @@ import {
   userRegisterReducer,
   userSigninReducer,
 } from "./reducers/userReducers";
+
 const initialState = {
   userSignin: {
     userInfo: localStorage.getItem("userInfo")
@@ -31,6 +33,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
+  orderCreate: orderCreateReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -38,4 +41,5 @@ const store = createStore(
   initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
+
 export default store;
