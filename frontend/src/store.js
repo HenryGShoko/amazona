8b24da562199aea411922f3460fabf3075845046
henry pvt <gshokx91@gmail.com>
2021-08-11 01:35:06 +0200

    Revert "Deliver order"
    This reverts commit 9f11f54f4d65d50b13c0cc8643b481c85850669f.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,6 @@ import { cartReducer } from "./reducers/cartReducers";
 import {
   orderCreateReducer,
   orderDeleteReducer,
-  orderDeliverReducer,
   orderDetailsReducer,
   orderListReducer,
   orderMineListReducer,
@@ -57,7 +56,6 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
   orderDelete: orderDeleteReducer,
-  orderDeliver: orderDeliverReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
